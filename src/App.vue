9cb1007e4dc48fb76fe2e9e68d65f899fbe5d3f3
kismatkc <kismatkc28@gmail.com>
2023-01-29 21:04:33 -0500

    your message

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>Kismat kc</h1>
+ <button @click="checkStatus" :disabled="isPlaying">Play</button>
+ <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Block.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block
+  },
+  data(){
+    return{
+  isPlaying: false,
+   delay: null
+
+    }
+  },
+  methods: {
+    checkStatus(){
+this.isPlaying = true
+this.delay = 2000 + Math.random() * 5000
+
+    }
   }
 }
 </script>
@@ -20,7 +34,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
